91241de5b3409fa530e056be4d84694e7e04e892
Kseniia Pukhova <xenio310390@gmail.com>
2024-07-04 10:49:20 +0300

    add loyalty page

--- js/mini-cart gifts.js
+++ js/mini-cart gifts.js
@@ -21,3 +21,39 @@
       },
     },
   });
+
+  // Range Input
+function SliderFun(ele) {
+  for (let i = 0; i < ele.length; i++) {
+    const element = ele[i];
+
+    const values = element.value;
+    const dataValue = element.getAttribute("max");
+    const fullValue = Math.round((values * 100) / dataValue);
+    element.nextSibling.parentNode.querySelector(".active-line").style.width =
+      fullValue + "%";
+    if (element.nextSibling.parentNode.querySelector(".active-dot")) {
+      element.nextSibling.parentNode.querySelector(".active-dot").style.left =
+        fullValue + "%";
+    }
+
+    // if (values % 3 === 0) {
+    console.log("The value is an integer.");
+    console.log("values", values / 3);
+    const vals = values / 3;
+    const ulList = element.parentNode.parentElement.querySelectorAll("ul li");
+    for (let ids = 0; ids < ulList.length; ids++) {
+      if (ids < vals || ids == vals) {
+        ulList[ids].classList.add("active");
+      } else {
+        ulList[ids].classList.remove("active");
+      }
+    }
+    // }
+  }
+}
+SliderFun($(".range-input input"));
+
+$(".range-input input").on("input", function () {
+  SliderFun($(this));
+});
