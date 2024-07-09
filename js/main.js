f5483da2d1a9c4c4982ee18c469284855480596e
Kseniia Pukhova <xenio310390@gmail.com>
2024-07-09 21:35:27 +0300

    fix loyalty

--- js/main.js
+++ js/main.js
@@ -848,24 +848,6 @@ function OpenSocial() {
     const dataValue = element.getAttribute("max");
     const fullValue = Math.round((values * 100) / dataValue);
     element.nextSibling.parentNode.querySelector(".active-line").style.setProperty("--range-size", fullValue + "%");
-    // if (element.nextSibling.parentNode.querySelector(".active-dot")) {
-    //   element.nextSibling.parentNode.querySelector(".active-dot").style.left =
-    //     fullValue + "%";
-    // }
-
-    // if (values % 3 === 0) {
-    // console.log("The value is an integer.");
-    // console.log("values", values / 3);
-    // const vals = values / 3;
-    // const ulList = element.parentNode.parentElement.querySelectorAll("ul li");
-    // for (let ids = 0; ids < ulList.length; ids++) {
-    //   if (ids < vals || ids == vals) {
-    //     ulList[ids].classList.add("active");
-    //   } else {
-    //     ulList[ids].classList.remove("active");
-    //   }
-    // }
-    // }
   }
 }
 SliderFun($(".range-input input"));
