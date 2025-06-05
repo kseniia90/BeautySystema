196c61248c16e13fb6e6471f8f5689b5ada2ee43
Kseniia Pukhova <xenio310390@gmail.com>
2025-06-05 16:44:28 +0300

    post page progress

--- js/main.js
+++ js/main.js
@@ -791,6 +791,25 @@ if (document.querySelector(".mini-cart-popup .auth-link") !== null) {
     });
 }
 
+//post-page-popup
+
+if (document.querySelector(".post-page-popup") !== null) {
+  document.querySelectorAll(".post-page-popup-close").forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.body.style.overflow = "visible";
+      document.querySelectorAll(".post-page-popup").forEach(el => el.classList.remove("active"));
+    });
+  });
+  document.addEventListener('click', function(event) {
+  document.querySelectorAll('.post-page-popup.active').forEach(function(popup) {
+    if (!popup.querySelector('.post-page-popup-content').contains(event.target)) {
+      popup.classList.remove('active');
+    }
+  });
+});
+}
+
 //show more-less start
 
 $.fn.showMore = function (options) {
